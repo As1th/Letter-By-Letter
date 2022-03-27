b7a6764b7730a38d0637cdc15081d8a578445290
As1th <asithumavin@gmail.com>
2022-03-27 18:16:14 +0530

    Better model metrics and evaluation tests

--- ImageModel/test_model_via_CSV.py
+++ ImageModel/test_model_via_CSV.py
@@ -3,22 +3,21 @@
 """
 import numpy as np
 import pandas as pd
-import cv2
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
 from keras.models import Sequential, load_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import layers
+from keras import metrics
 from keras.layers import *
 from keras.utils import np_utils
-from tqdm import tqdm
+import matplotlib.pyplot as plt
 import os
+from scikitplot.metrics import plot_confusion_matrix, plot_roc
 
 W = 28
 H = 28
 
-#edits test CSV images into usable form (see visualise_dataset.py for more info)
+#edits test CSV images into readable form (see visualise_dataset.py for more info)
 def reshape_and_rotate(image):
     image = image.reshape(W, H)
     image = np.fliplr(image)
@@ -36,3 +35,16 @@ y_test = np_utils.to_categorical(y_test, y_test.nunique())
 X_test = X_test.astype('float32') / 255
 X_test = X_test.reshape(-1,W, H, 1)
 model.evaluate(X_test, y_test)
+
+
+
+predictions = model.predict(X_test)
+confusion = tf.math.confusion_matrix(
+              labels = np.argmax(y_test, axis=1),      # get trule labels 
+              predictions = np.argmax(predictions, axis=1),  # get predicted labels 
+              num_classes=47)              # no. of classifier 
+print(confusion)
+
+plt.matshow(confusion)
+plt.colorbar()
+plt.show()
