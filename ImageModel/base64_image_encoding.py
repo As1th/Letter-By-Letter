293f39507389328c3bd86a356d677114b3b29786
As1th <86202152+As1th@users.noreply.github.com>
2022-03-31 18:25:17 +0530

    added some comments

--- ImageModel/base64_image_encoding.py
+++ ImageModel/base64_image_encoding.py
@@ -2,16 +2,17 @@ import base64
 
 import cv2
 image = cv2.imread('./ImageModel/test.png', cv2.IMREAD_ANYCOLOR)
+
+#encoding
 image = cv2.resize(image, (28, 28))
 cv2.imwrite('./ImageModel/test.png', image)
 
 with open('./ImageModel/test.png', "rb") as image_file:
     string = base64.b64encode(image_file.read())
 
-
+#decoding
 string = string.decode('utf-8')
 print(string)
-
 
 read = base64.b64decode(string)
 
